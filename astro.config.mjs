3aebd6914d9169f68bd749e4d74b14ade41eb9df
ArthurBottcher <arthurbottcher@gmail.com>
2026-07-20 19:45:05 -0300

    Adicionado SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://boladefogo.com.br',
+  integrations: [sitemap()],
 });
